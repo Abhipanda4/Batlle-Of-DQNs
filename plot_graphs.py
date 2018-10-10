35022066c563483e4d8410fb606961045f03750d
abhipanda4 <abhipanda03@gmail.com>
2018-10-10 07:50:50 +0530

    images added for whatever done yet

--- plot_graphs.py
+++ plot_graphs.py
@@ -10,6 +10,7 @@ file_paths = [root + a + "_stats.log" for a in algorithms]
 labels = []
 rewards = []
 losses = []
+
 for f in file_paths:
     if os.path.exists(f):
         df = pd.read_csv(f, header=None)
@@ -21,7 +22,7 @@ for f in file_paths:
         loss = np.asarray(df.iloc[1, 2:].values)
         losses.append(loss)
 
-def plot_graph(y_vals, x_label, y_label):
+def plot_graph(y_vals, x_label, y_label, f_name):
     for i in range(len(y_vals)):
         Y = y_vals[i][::5]
         X = list(range(len(Y)))
@@ -29,7 +30,8 @@ def plot_graph(y_vals, x_label, y_label):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend()
+    plt.savefig(f_name)
     plt.show()
 
-plot_graph(rewards, "Number of Episodes", "Reward obtained")
-plot_graph(losses, "Number of Episodes", "Training loss")
+plot_graph(rewards, "Number of Episodes", "Reward obtained", "images/reward_comparision.png")
+plot_graph(losses, "Number of Episodes", "Training loss", "images/loss_comparision.png")
